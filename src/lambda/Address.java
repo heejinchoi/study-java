38b4fc859b280c4f7734185ef256cc64b2035cfc
hjchoi <hjchoi@gaia3d.com>
2018-03-09 00:57:08 +0900

    commit

---
+++ src/lambda/Address.java
@@ -0,0 +1,19 @@
+package lambda;
+
+public class Address {
+	private String country;
+	private String city;
+	
+	public Address(String county, String city) {
+		this.country = county;
+		this.city = city;
+	}
+	
+	public String getCountry() {
+		return country;
+	}
+	public String getCity() {
+		return city;
+	}
+	
+}
