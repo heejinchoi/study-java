b49e5ee54e82ac0ef3ca6a26a073636dd857a586
hjchoi <hjchoi@gaia3d.com>
2018-05-22 23:15:34 +0900

    스트림-병렬처리

---
+++ src/ch16/stream/ParalleStreamExample.java
@@ -0,0 +1,27 @@
+package ch16.stream;
+
+import java.util.Arrays;
+import java.util.List;
+
+import ch16.stream.Student.Sex;
+
+public class ParalleStreamExample {
+	public static void main(String[] args) {
+		List<Student> totalList = Arrays.asList(
+			new Student("황미나", 50, Sex.FEMALE),
+			new Student("김수정", 90, Sex.FEMALE),
+			new Student("박수현", 70, Sex.FEMALE),
+			new Student("홍길동", 60, Sex.MALE)
+		);
+		
+		MaleStudent maleStudent = totalList.parallelStream()
+				.filter(s -> s.getSex() == Student.Sex.MALE)
+				.collect(MaleStudent :: new, MaleStudent :: accumulate, MaleStudent :: combine);
+		
+		maleStudent.getList().stream().forEach(s -> System.out.println(s.getName()));
+		
+		//실행 결과를 보면 처리를 담담하는 스레드는 main 스레드임을 알 수 있다.
+		
+	}
+
+}
