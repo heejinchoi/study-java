395c1e73f81d6d7de762044d49900f52ce4c7ae8
hjchoi <hjchoi@gaia3d.com>
2018-05-23 23:14:39 +0900

    javaFX - 라이프사이클, Scene

---
+++ src/ch17/javafx/AppMain.java
@@ -0,0 +1,17 @@
+package ch17.javafx;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class AppMain extends Application {
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		primaryStage.show();
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+	
+}
